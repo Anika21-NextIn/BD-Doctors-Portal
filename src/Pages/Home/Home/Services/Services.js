77894666a5218255e5320f4d4961cfe5e11b9dcc
Anika21-NextIn <anika21.nextin@gmail.com>
2022-05-07 23:28:49 +0600

    add fake data & services

--- src/Pages/Home/Home/Services/Services.js
+++ src/Pages/Home/Home/Services/Services.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import { experimentalStyled as styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import { Container } from '@mui/material';
+import Service from '../../Service/Service';
+import fluoride from '../../../../images/fluoride.png'
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+const services = [
+    {
+        name: 'Fluoride Treatment'
+        description: "jcbhjjkvb  hdscgyuhdsbv sdhfcvuhjdsgvi hgdcfvjgvik dhfcvyuhjgvyuhjc hjcvhjvgiujb"
+        img: fluoride
 
+    },
+
+    {
+        name: 'Cavity Filling'
+        description: "jcbhjjkvb  hdscgyuhdsbv sdhfcvuhjdsgvi hgdcfvjgvik dhfcvyuhjgvyuhjc hjcvhjvgiujb"
+        img: cavity
+
+    },
+
+    {
+        name: 'Teeth Whitening'
+        description: "jcbhjjkvb  hdscgyuhdsbv sdhfcvuhjdsgvi hgdcfvjgvik dhfcvyuhjgvyuhjc hjcvhjvgiujb"
+        img: whitening
+
+    }
+]
 
 const Services = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {Array.from(Array(6)).map((_, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
-              <Item>xs=2</Item>
-            </Grid>
-          ))}
-        </Grid>
+            <Container>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                 {Array.from(Array(6)).map((_, index) => (
+                <Grid item xs={2} sm={4} md={4} key={index}>
+                <Service></Service>
+                </Grid>
+                ))}
+                </Grid>
+            </Container>
       </Box>
     );
 };
