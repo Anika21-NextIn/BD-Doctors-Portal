ec029325fb20927c57be705cc2d3f0505b316055
Anika21-NextIn <anika21.nextin@gmail.com>
2022-05-11 00:53:52 +0600

    add service

--- src/Pages/Home/Home/Services/Services.js
+++ src/Pages/Home/Home/Services/Services.js
@@ -4,26 +4,30 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { Container } from '@mui/material';
 import Service from '../../Service/Service';
-import fluoride from '../../../../images/fluoride.png'
+import fluoride from '../../../../images/fluoride.png';
+import cavity from '../../../../images/cavity.png';
+import whitening from '../../../../images/whitening.png';
+
+
 
 const services = [
     {
-        name: 'Fluoride Treatment'
-        description: "jcbhjjkvb  hdscgyuhdsbv sdhfcvuhjdsgvi hgdcfvjgvik dhfcvyuhjgvyuhjc hjcvhjvgiujb"
+        name: 'Fluoride Treatment',
+        description: "jcbhjjkvb  hdscgyuhdsbv sdhfcvuhjdsgvi hgdcfvjgvik dhfcvyuhjgvyuhjc hjcvhjvgiujb",
         img: fluoride
 
     },
 
     {
-        name: 'Cavity Filling'
-        description: "jcbhjjkvb  hdscgyuhdsbv sdhfcvuhjdsgvi hgdcfvjgvik dhfcvyuhjgvyuhjc hjcvhjvgiujb"
+        name: 'Cavity Filling',
+        description: "jcbhjjkvb  hdscgyuhdsbv sdhfcvuhjdsgvi hgdcfvjgvik dhfcvyuhjgvyuhjc hjcvhjvgiujb",
         img: cavity
 
     },
 
     {
-        name: 'Teeth Whitening'
-        description: "jcbhjjkvb  hdscgyuhdsbv sdhfcvuhjdsgvi hgdcfvjgvik dhfcvyuhjgvyuhjc hjcvhjvgiujb"
+        name: 'Teeth Whitening',
+        description: "jcbhjjkvb  hdscgyuhdsbv sdhfcvuhjdsgvi hgdcfvjgvik dhfcvyuhjgvyuhjc hjcvhjvgiujb",
         img: whitening
 
     }
@@ -34,11 +38,13 @@ const Services = () => {
         <Box sx={{ flexGrow: 1 }}>
             <Container>
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                 {Array.from(Array(6)).map((_, index) => (
-                <Grid item xs={2} sm={4} md={4} key={index}>
-                <Service></Service>
-                </Grid>
-                ))}
+                {
+                    services.map(service =><Service
+                        key={service.name}
+                        service={service}
+                    ></Service>)
+                }
+               
                 </Grid>
             </Container>
       </Box>
